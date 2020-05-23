465d85fbf1dadb31ed3843395399fab9e20ad849
vjagadishvaranasi <jagadishvaranasi123@gmail.com>
2020-05-23 23:22:24 +0530

    state value assigned explicity without eve handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
 class SearchBar extends React.Component{
+    state={term:'kuvlulul'};
     render(){
-        return<div>SearchBar</div>
+        return(
+            <div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input type="text"  value={this.state.term}   />
+                        {/* if u put some value in state which is fixed for term that will will be renderd o screen and u cannot change anything 
+                        later as you did not wire a onchange eve handerler It will be read only mode */}
+                    </div>
+                </form>
+            </div>
+        )
     }
 }
 

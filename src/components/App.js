ae07c75455c052b0a647e86d74d906ab94a670f9
vjagadishvaranasi <jagadishvaranasi123@gmail.com>
2020-05-24 22:41:44 +0530

    fetching list number

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 import youtube from '../apis/youtube'
-
+const KEY='AIzaSyDoXxm4zYEwjM3OePvOPIPkNJQjI8omIcA';
 class App extends React.Component{
+state={videos:[]};
+
 //whenever use search for particulr term we nne make use of youtube api
 onTermSubmit=async term => {
   console.log(term);
@@ -15,11 +17,16 @@ onTermSubmit=async term => {
           key:KEY
        }
     });
+
+this.setState({videos:response.data.items});
 };
 
     render(){
         return (
-            <div className="ui container"><SearchBar onFormSubmit={this.onTermSubmit}/></div>
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit}/>
+                I have {this.state.videos.length} videos.
+                </div>
         );
     }
 }

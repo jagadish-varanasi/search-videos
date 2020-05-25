79b63500fe41866bee81c1f5f7d5fed5344de7d9
vjagadishvaranasi <jagadishvaranasi123@gmail.com>
2020-05-25 23:11:32 +0530

    added thumbnails path in object

--- src/components/videoItem.js
+++ src/components/videoItem.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 const VideoItem=({video})=>{
     //Now this component has one video object sent by VideoList...it has all details about individual video.....prop is video
-  return <div>
+  return( <div>
+
+    <img src={video.snippet.thumbnails.medium.url}/>  
     {video.snippet.title}
     {/* //u can obtain this title location in inspect.where u can know how that hirairchy is to reach title */}
-  </div>;
+  </div>
+  )
 }
 
 export default VideoItem;
